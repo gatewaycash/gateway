711c0267008294d1c9b1e34a8d297e320ac7d3b9
Alex Wilmer <djazium@gmail.com>
2018-11-24 17:53:24 -0500

    createButton page cleanup

--- modules/website/src/pages/CreateButton/index.js
+++ modules/website/src/pages/CreateButton/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import Paper from '@material-ui/core/Paper'
 import NavigationMenu from 'NavigationMenu'
-import PayButton from '@gateway/PayButton'
 import PreviewButton from './PreviewButton'
 import CreateButtonForm from './CreateButtonForm'
+import PaymentIDInfo from './PaymentIDInfo'
+import ClientCodeExample from './ClientCodeExample'
 
 class CreateButtonPage extends Component {
   state = {
@@ -83,58 +83,6 @@ class CreateButtonPage extends Component {
     }
   }
 
-  renderGeneratedCode = () => {
-    var buttonCode = '<div\n  class="payButton"\n'
-    buttonCode += '  merchantID="' + this.state.merchantID + '"\n'
-    if (this.state.buttonText.toString() !== 'Donate') {
-      buttonCode += '  buttonText="' + this.state.buttonText + '"\n'
-    }
-    if (this.state.amount !== '0' && this.state.anyAmount === false) {
-      buttonCode += '  amount="' + this.state.amount + '"\n'
-    }
-    if (
-      this.state.currency.toString().toLowerCase() !== 'bch' &&
-      this.state.anyAmount === false
-    ) {
-      buttonCode += '  currency="' + this.state.currency + '"\n'
-    }
-    if (this.state.dialogTitle.toString() !== 'Complete Your Payment') {
-      buttonCode += '  dialogTitle="' + this.state.dialogTitle + '"\n'
-    }
-    if (this.state.paymentID !== 'donation') {
-      buttonCode += '  paymentID="' + this.state.paymentID + '"\n'
-    }
-    if (
-      this.state.callbackURL !== 'None' &&
-      (this.state.callbackURL.toString().startsWith('https://') ||
-        this.state.callbackURL.toString().startsWith('http://'))
-    ) {
-      buttonCode += '  callbackURL="' + this.state.callbackURL + '"\n'
-    }
-    buttonCode += '></div>'
-    return (
-      <Paper className="paper">
-        <h2>Generated Code</h2>
-        <p>
-          Add this line of HTML once on each page you want to accept payments
-          on:
-        </p>
-        <pre className="sourceCode">
-          {`<script src="https://gateway.cash/pay.js"></script>`}
-        </pre>
-        <p>
-          Add this code wherever you want to place a payment button. You can put
-          as many buttons on the same page as you'd like:
-        </p>
-        <pre className="sourceCode">{buttonCode}</pre>
-        <p>
-          Feel free to change any of the values in the above code block except
-          your merchant ID, which is how you'll get paid.
-        </p>
-      </Paper>
-    )
-  }
-
   render() {
     return (
       <div className="container">
@@ -143,58 +91,16 @@ class CreateButtonPage extends Component {
           <CreateButtonForm />
         </div>
         <div className="rightPanel">
-          {this.renderGeneratedCode()}
+          <ClientCodeExample />
           <PreviewButton />
         </div>
         <div className="leftPanel">
           <div>
-            <Paper className="paper">
-              <h2>More Info About Payment IDs</h2>
-              <p>
-                The optional Payment ID property can be used to help you keep
-                track of different payments made to your merchant account. For
-                example, if you're selling T-shirts and you want to know who has
-                paid for their order, you can set a payment ID equal to your
-                order number. Since payment IDs show up in the View Payments
-                page, you'll know which orders have been paid for and are ready
-                to ship.
-              </p>
-            </Paper>
-            <Paper className="paper">
-              <h2>A Note on How Payments Are Processed</h2>
-              <p>
-                The site generates an address for each payment and securely
-                stores the key. The moment the customer pays, the Bitcoin Cash
-                network instantly validates the payment and within 3 seconds,
-                the customer is done and can move on with their day.
-              </p>
-              <p>
-                New payments are refreshed once every 10 minutes and forwarded
-                to merchants as soon as they are detected. About a tenth of a
-                cent is deducted from each payment to pay for the transfer fee,
-                and if you've chosen to help support the project (off by
-                default), your selected amount will also be deducted as well.
-                All other funds go to your address.
-              </p>
-            </Paper>
+            <PaymentIDInfo />
           </div>
         </div>
         <div className="rightPanel">
-          <Paper className="paper">
-            <h2>Support the Project</h2>
-            <p>
-              If you like this project and want to see it get even better for
-              both merchants and customers, please consider a donation.
-              Optionally, you can also choose to donate a portion of each
-              payment made to your merchant account from Settings.
-            </p>
-            <center>
-              <PayButton
-                merchantID="ef0fcea08bfa9cb0"
-                buttonText="Support gateway.cash"
-              />
-            </center>
-          </Paper>
+
         </div>
       </div>
     )
