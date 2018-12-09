7a3dcb4bf491c1037b0b5a6a4a2dd7e343851aea
Ty Everett <ty@tyweb.us>
2018-12-09 10:34:17 -0800

    Fix build errors in login endpoint

--- modules/api/login.js
+++ modules/api/login.js
@@ -4,63 +4,119 @@
  * @file Defines the /login API endpoint
  */
 const url = require('url')
+const bch = require('bitcore-lib-cash')
+
 module.exports = function (req, res) {
-    console.log('/login requested')
-    // parse the provided data
-    const query = url.parse(req.url, true).query
-    console.log(query)
-    // define a response object
-    const response = {}
-    if (!query.password) {
-      
-    }
-    } else if (req.session.address) {
-      // prefer logging in by address
-      var sql = 'select * from users where payoutAddress = ?'
-      conn.query(sql, [req.session.address], (err, result) => {
-        if (err) {
-          throw err
-        }
-        if (result.length !== 1) {
-          res.send('Incorrect address')
+  console.log('/login requested')
+
+  // parse the provided data
+  const query = url.parse(req.url, true).query
+  console.log(query)
+
+  // define a response object
+  const response = {}
+
+  // ensure a password was sent
+  if (!query.password) {
+    response.status = 'error'
+    response.error = 'No Password',
+    response.description = 'Provide a password when logging in'
+    res.end(JSON.stringify(response))
+
+  // verify that either an address or a username was provided
+  } else if (!query.address && !query.username) {
+    response.status = 'error'
+    response.error = 'Address or Username Required',
+    response.description = 'An address or a username is required to log in.'
+    res.end(JSON.stringify(response))
+
+  /*
+    Since we prefer to log in with an address (when both an address and a
+    username were provided), we check for an address first.
+  */
+  } else if (query.address) {
+
+    // search the database for records
+    var sql = `select password, salt, APIKey
+      from users
+      where
+      payoutAddress = ?
+      limit 1`
+    conn.query(sql, [query.address], (err, result) => {
+      if (err) {
+        throw err
+      }
+
+      // Fail unless the query returned exactly one record
+      if (result.length !== 1) {
+        response.status = 'error'
+        response.error = 'Invalid Login',
+        response.description = 'An incorrect address or password was given.'
+        res.end(JSON.stringify(response))
+
+      // validate the password
+      } else {
+        const user = result[0]
+        const passwordHash = sha256(query.password + user.salt)
+
+        // send the API key if the password matches
+        if (user.password === passwordHash) {
+          response.status = 'success'
+          response.APIKey = user.APIKey,
+          res.end(JSON.stringify(response))
+
+        // in all other cases, fail with invalid login
         } else {
-          var user = result[0]
-          var passwordHash = sha256(query.password + user.salt)
-          if (user.password === passwordHash) {
-            // login successful, set up session variables
-            req.session.address = user.payoutAddress
-            req.session.username = user.username
-            req.session.merchantID = user.merchantID
-            req.session.loggedIn = true
-            res.send('ok')
-          } else {
-            res.send('Incorrect password')
-          }
+          response.status = 'error'
+          response.error = 'Invalid Login',
+          response.description = 'An incorrect address or password was given.'
+          res.end(JSON.stringify(response))
         }
-      })
-    } else {
-      // search by username
-      var sql = 'select * from users where username = ?'
-      conn.query(sql, [req.session.username], (err, result) => {
-        if (err) {
-          throw err
-        }
-        if (result.length !== 1) {
-          res.send('Incorrect username')
+      }
+    })
+
+  /*
+    Try searching for the user by username only when the address was not
+    provided.
+   */
+  } else {
+    // search for records
+    var sql = `select password, salt, APIKey
+      from users
+      where
+      username = ?
+      limit 1`
+    conn.query(sql, [query.username], (err, result) => {
+      if (err) {
+        throw err
+      }
+
+      // fail unless there is exactly one match
+      if (result.length !== 1) {
+        response.status = 'error'
+        response.error = 'Invalid Login',
+        response.description = 'An incorrect username or password was given.'
+        res.end(JSON.stringify(response))
+
+      // validate the password
+      } else {
+        var user = result[0]
+        var passwordHash = sha256(query.password + user.salt)
+
+        // send the API key if the password matches
+        if (user.password === passwordHash) {
+          response.status = 'success'
+          response.APIKey = user.APIKey,
+          res.end(JSON.stringify(response))
+
+        // in all other cases, fail with invalid login
         } else {
-          var user = result[0]
-          var passwordHash = sha256(query.password + user.salt)
-          if (user.password === passwordHash) {
-            // login successful, set up session variables
-            req.session.address = user.payoutAddress
-            req.session.username = user.username
-            req.session.merchantID = user.merchantID
-            req.session.loggedIn = true
-            res.send('ok')
-          } else {
-            res.send('Incorrect password')
-          }
+          response.status = 'error'
+          response.error = 'Invalid Login',
+          response.description = 'An incorrect username or password was given.'
+          res.end(JSON.stringify(response))
         }
-      })
-    }
+      }
+    })
+  }
 }
