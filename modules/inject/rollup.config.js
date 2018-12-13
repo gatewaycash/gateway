374ed2c975005c32bfae0278a2e18a02179d3e47
Ty Everett <ty@tyweb.us>
2018-12-12 17:32:03 -0800

    remove workaround from inject.js

--- modules/inject/rollup.config.js
+++ modules/inject/rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
+import replace from 'rollup-plugin-replace'
 
 export default {
   input: 'inject.js',
@@ -16,5 +17,8 @@ export default {
     babel({
       exclude: '../../node_modules/**'
     }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify( 'production' )
+    })
   ],
 }
