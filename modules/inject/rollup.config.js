182d88c0b68d6493d5df586bed2ed608707e658a
Ty Everett <ty@tyweb.us>
2018-12-13 09:37:21 -0800

    add uglify to inject

--- modules/inject/rollup.config.js
+++ modules/inject/rollup.config.js
@@ -2,13 +2,13 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
 import replace from 'rollup-plugin-replace'
+import { uglify } from 'rollup-plugin-uglify'
 
 export default {
   input: 'inject.js',
   output: {
     file: './../website/public/pay.js',
-    format: 'umd',
-    sourcemap: true
+    format: 'iife'
   },
   plugins: [
     resolve({
@@ -20,6 +20,9 @@ export default {
     }),
     replace({
       'process.env.NODE_ENV': JSON.stringify( 'production' )
+    }),
+    uglify({
+      sourcemap: false
     })
   ],
 }
