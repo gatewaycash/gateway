f7de21c6e2bade3c3dbfcced7a60eb7afea2f3e5
Ty Everett <ty@tyweb.us>
2018-12-14 12:24:31 -0800

    remove react warnings

--- modules/paybutton/src/Dialog/index.js
+++ modules/paybutton/src/Dialog/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Dialog from '@material-ui/core/Dialog'
 import DialogContent from '@material-ui/core/DialogActions'
-import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 
 export default ({ open, title, handleClose, children, ...props }) => (
@@ -10,7 +9,7 @@ export default ({ open, title, handleClose, children, ...props }) => (
       <center>{title}</center>
     </DialogTitle>
     <DialogContent>
-      <DialogContentText>{children}</DialogContentText>
+      {children}
     </DialogContent>
   </Dialog>
 )
