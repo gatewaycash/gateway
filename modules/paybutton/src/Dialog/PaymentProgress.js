a509f02a714e187f1af1a65292ac46b49190b5be
Ty Everett <ty@tyweb.us>
2019-01-06 12:19:38 -0800

    Restore some sanity (#82)
    * Restore some sanity
    
    * Attempt fix
    
    * fix

--- modules/paybutton/src/Dialog/PaymentProgress.js
+++ modules/paybutton/src/Dialog/PaymentProgress.js
@@ -9,13 +9,7 @@ let PaymentProgress = ({
   hideAddressText
 }) => (
   <center>
-    <p
-      style={{
-        marginLeft: '0.5em',
-        marginRight: '0.5em',
-        marginTop: '-1.5em'
-      }}
-    >
+    <p>
       Send {amountBCH == 0 ? 'some' : amountBCH} Bitcoin&nbsp;Cash (BCH) to this
       address to complete your payment
     </p>
@@ -30,12 +24,15 @@ let PaymentProgress = ({
             paymentAddress
       }
       alt="Payment QR code"
+      title="Payment QR code"
+      poster="Payment QR code"
       style={{
         width: '15em',
         margin: 'auto',
         marginTop: '-1em',
-        marginBottom: '-1.5em',
-        align: 'center'
+        marginBottom: '-2em',
+        align: 'center',
+        clip: 'rect(10px,50px,50px,10px)'
       }}
     />
     {hideAddressText || (
--- modules/website/src/pages/portal/CreateButton/SupportProject.js
+++ modules/website/src/pages/portal/CreateButton/SupportProject.js
@@ -1,40 +1,21 @@
 import React from 'react'
-import Container from 'components/Container'
-import Text from 'components/Text'
+import { Container, Text } from 'components'
+import PayButton from '@gatewaycash/paybutton'
 
-/*
-
-  TODO:
-  This is broken and a hack is used to make it work.
-  We need to find out why the React component is failing.
-  We need to remove the <script> tag from the modules/website/public/index.html
-  file so that we don't rely on compiled code.
-
-*/
-
-export default () => {
-  setTimeout(() => {
-    window.PayButton && window.PayButton.render(
-      'gatewayContribute',
-      {
-        merchantID: 'ef0fcea08bfa9cb0',
-        buttonText: 'Support Gateway.cash'
-      }
-    )},
-  1000
-  )
-  return (
-    <Container>
-      <h2>Support the Project</h2>
-      <Text>
-        If you like this project and want to see it get even better for both
-        merchants and customers, please consider a donation. Optionally, you can
-        also choose to donate a portion of each payment made to your merchant
-        account from Settings.
-      </Text>
-      <center>
-        <div id="gatewayContribute"></div>
-      </center>
-    </Container>
-  )
-}
+export default () => (
+  <Container>
+    <h2>Support the Project</h2>
+    <Text>
+      If you like this project and want to see it get even better for both
+      merchants and customers, please consider a donation. Optionally, you can
+      also choose to donate a portion of each payment made to your merchant
+      account from Settings.
+    </Text>
+    <center>
+      <PayButton
+        address='bitcoincash:pz3txlyql9vc08px98v69a7700g6aecj5gc0q3xhng'
+        buttonText='Support Gateway.cash'
+      />
+    </center>
+  </Container>
+)
