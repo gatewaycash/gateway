4d617652cc91211d9ab7db38b23eea64d1106bc6
Ty Everett <ty@tyweb.us>
2019-01-06 12:08:22 -0800

    fix

--- modules/inject/rollup.prod.js
+++ modules/inject/rollup.prod.js
@@ -5,40 +5,51 @@ import replace from 'rollup-plugin-replace'
 import json from 'rollup-plugin-json'
 import builtins from 'rollup-plugin-node-builtins'
 import globals from 'rollup-plugin-node-globals'
-import { uglify } from 'rollup-plugin-uglify'
+//import { uglify } from 'rollup-plugin-uglify'
 
 export default {
   input: 'inject.js',
   output: {
     file: './../website/public/pay.js',
     format: 'iife',
-    sourcemap: false
+    sourcemap: true,
+    global: {}
   },
   plugins: [
-    commonjs({
-      include: '../../node_modules/**'
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+      preferBuiltins: true
     }),
+    commonjs({ include: '../../node_modules/**' }),
     babel({
       exclude: '../../node_modules/**',
       babelrc: false,
       presets: [
-        ['@babel/env', {
-          modules: false
-        }],
+        [
+          '@babel/env',
+          {
+            modules: false,
+            targets: {
+              chrome: '58',
+              ie: '11'
+            },
+            useBuiltIns: 'usage'
+          }
+        ],
         '@babel/preset-react'
+      ],
+      plugins: [
+        '@babel/transform-regenerator',
+        '@babel/plugin-external-helpers'
       ]
-    }),
-    resolve({
-      browser: true,
-      jsnext: true,
-      preferBuiltins: false
     }),
     json(),
     builtins(),
     globals(),
     replace({
       'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    //uglify()
+    })
   ]
 }
