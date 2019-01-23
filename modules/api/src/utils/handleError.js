aea24dbabe40916686d673f07fd794910e1d651b
Ty Everett <ty@tyweb.us>
2019-01-22 17:00:42 -0800

    Add nicer console output

--- modules/api/src/utils/handleError.js
+++ modules/api/src/utils/handleError.js
@@ -3,12 +3,15 @@
  * @author The Gateway Project Developers <hello@gateway.cash>
  * @file Provides a handler for API errors
  */
+import prettyjson from 'prettyjson'
+
 export default (error, description, res) => {
-  if (!res) return false
   let response = {}
   response.status = 'error'
   response.error = error
   response.description = description
+  console.log(prettyjson.render(response, {keysColor: 'red'}))
+  if (!res) return false
   res.status(400).end(JSON.stringify(response))
   return false
 }
--- modules/api/src/utils/handleResponse.js
+++ modules/api/src/utils/handleResponse.js
@@ -3,9 +3,11 @@
  * @author The Gateway Project Developers <hello@gateway.cash>
  * @file Builds and sends the API response
  */
+import prettyjson from 'prettyjson'
+
 export default (response, res) => {
   response.status = 'success'
-  console.log(response)
+  console.log(prettyjson.render(response))
   res.status(200).end(JSON.stringify(response))
   return true
 }
